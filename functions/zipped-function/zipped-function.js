45c263e65593ec98f171d7aaab675213eea614ed
Dennis Padiernos <depadiernos@gmail.com>
2020-02-19 08:02:00 -0600

    gitignore, functional function, etc.

---
+++ functions/zipped-function/zipped-function.js
@@ -0,0 +1,22 @@
+const axios = require("axios")
+
+export function handler(event, context, callback) {
+
+  // Perform the API call.
+  const get = () => {
+    axios.get("https://icanhazdadjoke.com/")
+    .then((response) =>
+      {
+        console.log(response.data)
+        callback( null, {
+          statusCode: 200,
+          body: JSON.stringify(response.data)
+        })
+      }
+    )
+    .catch(err => pass(err))
+  }
+  if(event.httpMethod == 'GET'){
+    get()
+  };
+};
