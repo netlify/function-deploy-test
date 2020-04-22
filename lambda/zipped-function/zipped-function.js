5e523a8c19ea8ddea7523d8d850069ff2aa032e0
Dennis Padiernos <depadiernos@gmail.com>
2020-04-22 07:30:21 -0500

    Update zipped-function.js

--- lambda/zipped-function/zipped-function.js
+++ lambda/zipped-function/zipped-function.js
@@ -1,12 +1,15 @@
 const axios = require("axios")
 const path = require("path")
 const fs = require("fs")
+const outside = require("../outside.js")
 
 // path of the included file will be `process.env.LAMBDA_TASK_ROOT/{name_of_function}/{included_filename}`
 const fileName = "./zipped-function/some-other.js"
 const resolved = (process.env.LAMBDA_TASK_ROOT)? path.resolve(process.env.LAMBDA_TASK_ROOT, fileName):path.resolve(__dirname, fileName)
 
 fs.readFile(resolved, "utf8", (err, results) => console.log(results))
+
+console.log(outside.message)
 
 exports.handler = function(event, context, callback) {
   // Perform the API call.
